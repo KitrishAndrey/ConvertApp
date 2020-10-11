3f0c7c07ca5d5d1de4a025f68b9a397aa46ed0ed
Simple-Coder964 <63211791+Simple-Coder964@users.noreply.github.com>
2020-10-11 23:50:08 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,11 +3,12 @@ from tkinter import *
 import os
 
 print("Program Started")
+#path = r"C:\Users\User\Desktop\Files" # path of folder with files to convert for test
 
 root = Tk()
 
 root.title(u'Convert_app')
-root.geometry('500x400+700+300')
+root.geometry('500x400+700+300')# ширина=300, высота=150, x=300, y=200
 root.iconname("Convert_App")
 
 label = Label(root, text="Folder Path", height=1, width=20)
@@ -34,17 +35,27 @@ def getnameoffolder():
 def create_new_folder():
     os.mkdir("Ready_Vidios")
 
+counter = 0
 def main(event):
-    create_new_folder()
-    path = getnameoffolder()
-    file_list = os.listdir(r"{}".format(path[:-1])) 
-    counter = 0
-    for f in file_list:
-        counter += 1
-        convertfunc(os.path.join(r"{}".format(path[:-1]), f), counter)
-    print("Successfully Converted")
+    global counter
+    if counter != 0 and os.path.exists("Ready_Vidios"):
+        counter = counter
+        path = getnameoffolder()
+        file_list = os.listdir(r"{}".format(path[:-1]))  # список файлов и папок в директории, где запущена программа
+        for f in file_list:
+            counter += 1
+            convertfunc(os.path.join(r"{}".format(path[:-1]), f), counter)
+        print("Successfully Converted")
+    else:
+        create_new_folder()
+        path = getnameoffolder()
+        file_list = os.listdir(r"{}".format(path[:-1]))  # список файлов и папок в директории, где запущена программа
+        for f in file_list:
+            counter += 1
+            convertfunc(os.path.join(r"{}".format(path[:-1]), f), counter)
+            print("Successfully Converted")
 
-conv_button.bind("<Button-1>", main) 
+conv_button.bind("<Button-1>", main) #при нажатии ЛКМ на кнопку вызывается функция main
 
 label.pack(side=TOP, pady=10)
 folder_path.pack(side=TOP, padx=5, pady=50)
